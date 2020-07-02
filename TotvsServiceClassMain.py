efd8753914c5ac95d58a96c4c5aefb3ffed077a1
gabrieldss808 <gabrieldss808@gmail.com>
2020-07-01 22:05:31 -0300

    Aprendendo Mais Sobre o Tkinter e estruturando o app

--- TotvsServiceClassMain.py
+++ TotvsServiceClassMain.py
@@ -1,6 +1,12 @@
 from tkinter import Tk
+from utils.topMenu import TopMenu
+from tkinter import BOTH
+from tkinter.ttk import Button, Style
+
 
 class TotvsService3(Tk):
+
+    topMenu = TopMenu   
 
     def configTotvsServiceLayoyt(self):
 
@@ -11,10 +17,14 @@ class TotvsService3(Tk):
         self["bg"] = "#616161"
 
         self.iconbitmap("icons/icon.ico")
+
+        self.LoadPainels()
+
+        self.bind("<Configure>",self.callback)
     
     def Positioninthecenter(self):
 
-        largura = 450
+        largura = 600
         altura = 600
 
         largura_tela = self.winfo_screenwidth()
@@ -24,3 +34,19 @@ class TotvsService3(Tk):
         posy = altura_tela/2 - altura/2
 
         self.geometry("%dx%d+%d+%d" % (largura, altura, posx, posy))
+
+    def LoadPainels(self):
+
+        self.topMenu = TopMenu(self)
+        self.configTopMenu()
+        self.topMenu.pack()
+
+    def callback(self,event):
+
+        self.configTopMenu()
+
+    def configTopMenu(self):
+
+        self.topMenu["height"] = self.winfo_height()*0.12
+        self.topMenu["width"] = self.winfo_width()
+        self.topMenu["bg"] = "#AFB42B"
---
+++ utils/topMenu.py
@@ -0,0 +1,6 @@
+from tkinter import PanedWindow
+from tkinter import Button
+
+class TopMenu(PanedWindow):
+    
+    pass        
